c7e939a13d5307f3b5184000523f9cb65092b13f
Lomi <54989881+xxxagent350@users.noreply.github.com>
2024-11-20 18:02:57 +0300

    добавлена реализация острова по умолчанию

--- models/world_objects/island.py
+++ models/world_objects/island.py
@@ -1,4 +1,8 @@
+from core.vector2 import Vector2
+
 
 class Island:
-    def __init__(self, local_points):
+    def __init__(self, local_points=None):
+        if local_points is None:
+            local_points = [Vector2(0, 0)]
         self.local_points = local_points
