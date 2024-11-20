02557b4c73c770d03b223a3da2d79e684c87d696
Lomi <54989881+xxxagent350@users.noreply.github.com>
2024-11-20 12:34:30 +0300

    класс user

--- models/user.py
+++ models/user.py
@@ -1,1 +1,10 @@
+
 # Класс пользователя, хранящий его достижения, настройки, и т. д.
+class User:
+    def __init__(self, user_id, name = "No name", artefacts=None):
+        if artefacts is None:
+            artefacts = []
+        self.id = user_id
+        self.name = name
+        self.artefacts = artefacts
+        self.special_info = ""
